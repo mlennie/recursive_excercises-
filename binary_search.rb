5c844c58ab2995185c8ae5dcf0ac37ab4d37f508
Monty Lennie <montylennie@gmail.com>
2018-09-25 22:16:51 +0000

    add binary_search recursive approach

---
+++ binary_search.rb
@@ -0,0 +1,29 @@
+def findIndex(array, number, index=0)
+  # get middle value
+  middle_value_index = (array.length - 1 ) / 2
+  middle_value = array[middle_value_index]
+
+  # return value index if matches
+  return index + middle_value_index if middle_value == number
+
+  # return not present if not present
+  return "not present" if middle_value_index == -1
+
+  # if middle value greater, check in first half of array
+  if middle_value > number
+    new_array = array.slice(0, middle_value_index)
+  end
+  # if middle value less than number, check in second half of array
+  if middle_value < number
+    new_array = array.slice(middle_value_index + 1, array.length)
+    index += middle_value_index + 1
+  end
+
+  # rerun findIndex with new array and index
+  return findIndex(new_array, number, index)
+end
+
+number = 4
+array = [-1,  0,  1, 1, 3, 4, 5, 6, 8, 9, 10, 10, 11, 20, 100]
+index = findIndex(array, number)
+print(index)
